b440327c378f45384d745d99db45a95918041be7
Oussama CHERIFI ALAOUI <oussamac@padok.fr>
2023-05-12 15:45:48 +0200

    feat: introduce breaking change

--- foobar/foobar.go
+++ foobar/foobar.go
@@ -7,7 +7,7 @@ import (
 
 // Sequence returns a sequence of numbers from 1 to length, where multiples of
 // 3 are replaced by "foo", multiples of 5 are replaced by "bar", and multiples
-// of both 3 and 5 are rpelaced by "foobar".
+// of both 3 and 5 are replaced by "foobar".
 func Sequence(length int) ([]string, error) {
 	if length < 0 {
 		return nil, errors.New("length is negative")
@@ -20,7 +20,7 @@ func Sequence(length int) ([]string, error) {
 		switch {
 		case n%3 == 0 && n%5 == 0:
 			seq[i] = "foobar"
-		case n%5 == 0:
+		case n%7 == 0:
 			seq[i] = "bar"
 		case n%3 == 0:
 			seq[i] = "foo"
